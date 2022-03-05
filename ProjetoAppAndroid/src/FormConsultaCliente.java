85db1b3af65609f342005d67ed3d74d237e4b508
Lucas Schmidt <lucas.schmidt015@gmail.com>
2022-03-05 16:24:34 -0300

    Nao sei

--- ProjetoAppAndroid/src/FormConsultaCliente.java
+++ ProjetoAppAndroid/src/FormConsultaCliente.java
@@ -35,30 +35,27 @@ public class FormConsultaCliente extends Principal {
 
     public void cadastrarCliente()
     {
-        try{
-            Cliente C = new Cliente();
+        
+        Cliente C = new Cliente();
 
-            System.out.println("Digite o id do cliente");
-            C.setId(scan.nextInt());
-    
-            System.out.println("Digite o nome do cliente: ");
-            C.setNome(scan.nextLine());
-    
-            System.out.println("Digite a idade do cliente: ");
-            C.setIdade(scan.nextInt());        
-    
-            System.out.println("Digite o CPF do cliente: ");
-            C.setCPF(scan.nextInt());
-    
-            System.out.println("=-=-=-=-=-=-=-=-=-=-=-Vamos Agora Informar a Data De Nascimento do Cliente=-=-=-=-=-=-=-=-=-=-=-=-=");
-            C.setNascimento(informarData());
-    
-            ClientesCadastrados.add(C);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Erro ao cadastrar");
-        }
+        System.out.println("Digite o id do cliente");
+        C.setId(scan.nextInt());
+
+        System.out.println("Digite o nome do cliente: ");
+        String nome = scan.nextLine();
+        C.setNome(nome);
+
+        System.out.println("Digite a idade do cliente: ");
+        C.setIdade(scan.nextInt());        
+
+        System.out.println("Digite o CPF do cliente: ");
+        C.setCPF(scan.nextInt());
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-Vamos Agora Informar a Data De Nascimento do Cliente=-=-=-=-=-=-=-=-=-=-=-=-=");
+        C.setNascimento(informarData());
+
+        ClientesCadastrados.add(C);
+        
     }
 
 
